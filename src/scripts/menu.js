082acdeb7932604ea1cf8f5515450bb864a97dad
Keenan Kunzelman <keenan@cyberxelite.com>
2024-08-28 12:50:20 -0400

    added mobile nav bar. have a problem that is fucking up regular nav bar though. It has to do with the svg getting loaded for teh hamburger. I need to remove it from the dom tree till I need it?

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,33 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
+
+document.querySelector('.fa-bars').addEventListener('click', () => {
+
+
+  var navs = document.getElementsByClassName('navbar-nav');
+  
+  if (navs[0].style.visibility === "visible"){
+
+    for (var i = 0; i < navs.length; ++i) {
+      navs[i].style.visibility = "hidden";
+
+    }
+  }else{
+    for (var i = 0; i < navs.length; ++i) {
+      navs[i].style.backgroundColor = "#23232e";
+      navs[i].style.visibility = "visible";
+
+    }
+    document.querySelector('main').style.paddingTop = "6.5rem";
+  }
+
+
+
+
+  setTimeout(function(){
+  document.querySelector('.fa-bars').classList.toggle('fa-flip');
+  },500);
+
+  document.querySelector('.fa-bars').classList.toggle('fa-flip');
+
 });
+
+
